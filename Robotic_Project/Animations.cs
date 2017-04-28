d05b95e67710388d0276cb660a2e59111d567a8d
Çağrı Eroğlu <eroglu1994@gmail.com>
2017-04-28 22:27:05 +0300

    Gauge Control

---
+++ Robotic_Project/Animations.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robotic_Project;
+using System.Windows.Media.Animation;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Robotic_Project
+{
+    class Animations
+    {
+        //Global Variables
+        MainWindow mw = (MainWindow)System.Windows.Application.Current.MainWindow;
+        UIElement[] GaugeBar1 = new UIElement[10];
+        UIElement[] GaugeBar2 = new UIElement[10];
+
+        public void Initialize()
+        {
+            //FOR GAUGE 1
+            GaugeBar1[0] = mw.GaugeBar_1;
+            GaugeBar1[1] = mw.GaugeBar_2;
+            GaugeBar1[2] = mw.GaugeBar_3;
+            GaugeBar1[3] = mw.GaugeBar_4;
+            GaugeBar1[4] = mw.GaugeBar_5;
+            GaugeBar1[5] = mw.GaugeBar_6;
+            GaugeBar1[6] = mw.GaugeBar_7;
+            GaugeBar1[7] = mw.GaugeBar_8;
+            GaugeBar1[8] = mw.GaugeBar_9;
+            GaugeBar1[9] = mw.GaugeBar_10;
+
+
+            //FOR GAUGE 2
+            GaugeBar2[0] = mw.GaugeBar_11;
+            GaugeBar2[1] = mw.GaugeBar_12;
+            GaugeBar2[2] = mw.GaugeBar_13;
+            GaugeBar2[3] = mw.GaugeBar_14;
+            GaugeBar2[4] = mw.GaugeBar_15;
+            GaugeBar2[5] = mw.GaugeBar_16;
+            GaugeBar2[6] = mw.GaugeBar_17;
+            GaugeBar2[7] = mw.GaugeBar_18;
+            GaugeBar2[8] = mw.GaugeBar_19;
+            GaugeBar2[9] = mw.GaugeBar_20;
+        }
+
+        public void doGaugeAnimation(double currentNumber, int selectedGauge)
+        {
+            var GAUGE = new UIElement[10];
+            if (selectedGauge == 1)
+            {
+                GAUGE = GaugeBar1;
+            }
+            else
+            {
+                GAUGE = GaugeBar2;
+            }
+
+            int num = Convert.ToInt32(currentNumber);
+
+
+            if (num == 10)
+            {
+
+            }
+
+            if (num > 9.5 || num == 0)
+            {
+                Storyboard sb = mw.FindResource("SelectedGaugeUP") as Storyboard;
+                Storyboard.SetTarget(sb, GAUGE[0]);
+                sb.Begin();
+
+                for (int i = 0; i <= 9; i++)
+                {
+                    if (i != num)
+                    {
+                        sb = mw.FindResource("SelectedGaugeDOWN") as Storyboard;
+                        Storyboard.SetTarget(sb, GAUGE[i]);
+                        sb.Begin();
+                        //mw.LogMessage(i.ToString() + " down");
+                    }
+                }
+            }
+            else
+            {
+                Storyboard sb = mw.FindResource("SelectedGaugeDOWN") as Storyboard;
+                Storyboard.SetTarget(sb, GAUGE[0]);
+                sb.Begin();
+
+
+
+                sb = mw.FindResource("SelectedGaugeUP") as Storyboard;
+                Storyboard.SetTarget(sb, GAUGE[num]);
+                sb.Begin();
+
+                for (int i = 0; i <= 9; i++)
+                {
+                    if (i != num)
+                    {
+                        sb = mw.FindResource("SelectedGaugeDOWN") as Storyboard;
+                        Storyboard.SetTarget(sb, GAUGE[i]);
+                        sb.Begin();
+                        //mw.LogMessage(i.ToString() + " down");
+                    }
+                }
+            }
+        }
+
+        public void RotateNeedle(double newValue, UIElement _Object)
+        {
+            var currentAngle = mw.CurrentRotationAngle(_Object);
+
+
+            var dist = Math.Abs(mw.DegreeToDoubleValue(currentAngle) - newValue);
+            var time = (int)(dist * 100000 / 750);
+            var Animation = new DoubleAnimation(currentAngle, mw.DoubleValueToDegree(newValue), new Duration(new TimeSpan(0, 0, 0, 0, time)));
+            var rT = new RotateTransform();
+            _Object.RenderTransform = rT;
+            //Animation.RepeatBehavior = Timeline.RepeatBehaviorProperty(1);
+            rT.BeginAnimation(RotateTransform.AngleProperty, Animation);
+        }
+
+        public void RandomNeedleMovement(UIElement NeedleOBJ)
+        {
+            var randomNewValue = (new Random().NextDouble() * (9 - 0) + 0);
+            RotateNeedle(randomNewValue, NeedleOBJ);
+            mw.LogMessage("New Random Value: " + randomNewValue.ToString("0.0"));
+        }
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Robotic_Project/MainWindow.xaml.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using MahApps.Metro.Controls;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Robotic_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public void metroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LogMessage("Program Started!");
+            StartTimer();
+            Animations temp_Ah = new Animations();
+            temp_Ah.Initialize();
+            ah = temp_Ah;
+        }
+
+        DispatcherTimer Timer = new DispatcherTimer();
+        public void StartTimer()
+        {
+            Timer.Tick += new EventHandler(Timer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            Timer.Start();
+        }
+        public async void Timer_Tick(object sender, EventArgs e)
+        {
+            await doUIRefresh();
+        }
+
+        Animations ah = new Animations();
+
+        public async Task doUIRefresh()
+        {
+            //Refresh Gauge 1
+            var angle = CurrentRotationAngle(needle);
+            double num = DegreeToDoubleValue(angle);
+            tbxClockValue_1.Text = num.ToString("0.0");
+            if (Convert.ToDouble(tbxClockValue_1.Text) >= 10)
+            {
+                tbxClockValue_1.Text = "9.9";
+            }
+            ah.doGaugeAnimation(num, 1);
+
+
+            //Refresh Gauge 2
+            angle = CurrentRotationAngle(needle_2);
+            num = DegreeToDoubleValue(angle);
+            tbxClockValue_2.Text = num.ToString("0.0");
+            if (Convert.ToDouble(tbxClockValue_2.Text) >= 10)
+            {
+                tbxClockValue_2.Text = "9.9";
+            }
+            ah.doGaugeAnimation(num, 2);
+
+
+        }
+
+        public double CurrentRotationAngle(UIElement deneme)
+        {
+            double angle = 0.0;
+
+            Transform transform = deneme.RenderTransform;
+
+            if (transform is TransformGroup)
+            {
+                TransformGroup tg = (TransformGroup)deneme.RenderTransform;
+                foreach (Transform t in tg.Children)
+                    if (t is RotateTransform)
+                    {
+                        RotateTransform r = (RotateTransform)t;
+                        angle += r.Angle;
+                    }
+            }
+
+            if (transform is RotateTransform)
+            {
+                angle = ((RotateTransform)transform).Angle;
+            }
+
+            return angle;
+
+        }
+
+        public double DegreeToDoubleValue(double Angle)
+        {
+            if (Angle == 360)
+            {
+                return 0;
+            }
+            else
+            {
+                return Angle / 36;
+            }
+
+        }
+
+        public double DoubleValueToDegree(double Value)
+        {
+            if (Value == 0)
+            {
+                return 360;
+            }
+            else
+            {
+                return Value * 36;
+            }
+
+        }
+
+        public void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbxGauge1.Text != "")
+            {
+                if (Convert.ToDouble(tbxGauge1.Text) >= 0 && Convert.ToDouble(tbxGauge1.Text) <= 9.999)
+                {
+                    var value = Convert.ToDouble(tbxGauge1.Text);
+                    ah.RotateNeedle(value, needle);
+                    LogMessage("New Manual Value: " + value);
+                    tbxGauge1.Text = "";
+                }
+                else
+                {
+                    LogMessage("Enter Only '0' to '9'");
+                }
+
+            }
+            else
+            {
+                ah.RandomNeedleMovement(needle);
+            }
+
+        }
+
+        public void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbxGauge2.Text != "")
+            {
+                if (Convert.ToDouble(tbxGauge2.Text) >= 0 && Convert.ToDouble(tbxGauge2.Text) <= 9.999)
+                {
+                    var value = Convert.ToDouble(tbxGauge2.Text);
+                    ah.RotateNeedle(value, needle_2);
+                    LogMessage("New Manual Value: " + value);
+                    tbxGauge2.Text = "";
+                }
+                else
+                {
+                    LogMessage("Enter Only '0' to '9'");
+                }
+            }
+            else
+            {
+                ah.RandomNeedleMovement(needle_2);
+            }
+        }
+
+        public void LogMessage(string Message, string type = "Default")
+        {
+            var currentTime = DateTime.Now;
+            var content = "[" + currentTime.ToLongTimeString() + "]" + "  " + Message + "\n";
+            richTextBox.AppendText(content);
+            richTextBox.ScrollToEnd();
+        }
+
+        private void toggleSwitchButton_Checked(object sender, RoutedEventArgs e)
+        {
+            tbxbluetoothState.Text = "ONLINE";
+            tbxbluetoothState.Foreground = Brushes.LimeGreen;
+            LogMessage("Bluetooth ONLINE");
+
+        }
+
+        private void toggleSwitchButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            tbxbluetoothState.Text = "OFFLINE";
+            tbxbluetoothState.Foreground = Brushes.Gray;
+            LogMessage("Bluetooth OFFLINE");
+        }
+
+        public SerialPort serialPort = new SerialPort();
+        int counter = 0;
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            while (serialPort.IsOpen)
+            {
+                // WRITE THE INCOMING BUFFER TO CONSOLE
+                while (serialPort.BytesToRead > 0)
+                {
+                    LogMessage(Convert.ToChar(serialPort.ReadChar()).ToString());
+                }
+                // SEND
+                serialPort.WriteLine("PC counter: " + (counter++));
+                Thread.Sleep(500);
+            }
+        }
+
+        private void button1_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM8"; // Set in Windows
+            serialPort.Open();
+
+            Arduino_Timer.Tick += new EventHandler(Arduino_Timer_Tick);
+            Arduino_Timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            Arduino_Timer.Start();
+
+        }
+
+        private async void Arduino_Timer_Tick(object sender, EventArgs e)
+        {
+            await ArduinoData();
+        }
+
+        DispatcherTimer Arduino_Timer = new DispatcherTimer();
+        private async Task ArduinoData()
+        {
+            while (serialPort.IsOpen)
+            {
+                // WRITE THE INCOMING BUFFER TO CONSOLE
+                while (serialPort.BytesToRead > 0)
+                {
+                    LogMessage(Convert.ToChar(serialPort.ReadChar()).ToString());
+                }
+                // SEND
+                serialPort.WriteLine("PC counter: " + (counter++));
+
+                //await Task.Delay(500);
+            }
+        }
+    }
+}
+
